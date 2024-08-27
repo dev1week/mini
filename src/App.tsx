5bd5023d234286a3876f2abcf1019de605ab9924
hanju <rondo2860@gmail.com>
2024-08-27 12:01:23 +0900

    feat: main 페이지 fetch 기능과 loading 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+
+
 import './scss/global.scss';
+import classNames from 'classnames/bind'
+import styles from './App.module.scss'
+
+const cx = classNames.bind(styles)
 function App() {
+  const [wedding, setWedding] = useState(null); 
+  const [loading, setLoading] = useState(true); 
+
+  useEffect(()=>{
+    //비동기에서 데이터를 보장받을 수 있는 방법 
+      //callback, promise, async-await
+    fetch('http://localhost:8888/wedding').then((response)=>{
+      return response.json()
+    }).then((data)=>{
+      setWedding(data); 
+      setLoading(false);
+    })
+  }, [])
+
+
+  if(loading){
+    return <div>Loading</div>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={cx('container')}>
+      {JSON.stringify(wedding)}
     </div>
   );
 }
