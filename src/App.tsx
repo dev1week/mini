27242022a1e668eff974080717f50b2f99d689e0
hanju <rondo2860@gmail.com>
2024-08-27 12:10:22 +0900

    feat:  fetch 함수 내 404 error 처리

--- src/App.tsx
+++ src/App.tsx
@@ -10,21 +10,37 @@ const cx = classNames.bind(styles)
 function App() {
   const [wedding, setWedding] = useState(null); 
   const [loading, setLoading] = useState(true); 
+  const [error, setError] = useState(false); 
 
   useEffect(()=>{
     //비동기에서 데이터를 보장받을 수 있는 방법 
       //callback, promise, async-await
-    fetch('http://localhost:8888/wedding').then((response)=>{
+    fetch('http://localhost:8888/wedding22').then((response)=>{
+      if(response.ok===false){
+        throw new Error('청첩장 정보를 불러오지 못했습니다.')
+
+        return response.json(); 
+      }
+
       return response.json()
     }).then((data)=>{
       setWedding(data); 
       setLoading(false);
+    })
+    .catch((e)=>{
+      console.log('에러가 발생하였습니다.', e);
+      setError(true); 
+    }).finally(()=>{
+      setLoading(false)
     })
   }, [])
 
 
   if(loading){
     return <div>Loading</div>
+  } 
+  if(error){
+    return <div>error</div>  
   }
 
   return (
